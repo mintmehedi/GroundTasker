902d4f51105abf0782ab0f18cf857e70003a8deb
Mehedi Hasan <mintmehedi@gmail.com>
2025-06-14 20:09:58 +1000

    Temporary auto-create superuser

---
+++ csci334_platform/__init__.py
@@ -0,0 +1,19 @@
+import django
+from django.contrib.auth import get_user_model
+from django.db.utils import OperationalError
+
+django.setup()
+
+try:
+    User = get_user_model()
+    if not User.objects.filter(username="admin").exists():
+        User.objects.create_superuser(
+            username="admin",
+            email="admin@example.com",
+            password="admin1234"
+        )
+        print("✅ Superuser created: admin / admin1234")
+    else:
+        print("Superuser already exists.")
+except OperationalError:
+    print("Database not ready yet.")
